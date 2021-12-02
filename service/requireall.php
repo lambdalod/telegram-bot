85ce5328bf75661b120f21e3b5e47ee1112ed0ef
Duangellow <36920878+DuangellowX@users.noreply.github.com>
2021-12-02 22:30:42 +0300

    basic user class

--- service/requireall.php
+++ service/requireall.php
@@ -5,3 +5,4 @@
 
 require_once 'database.php'; // Database connection
 require_once '../bot/api.php'; // API methods
+require_once 'user.php'; // User
---
+++ service/user.php
@@ -0,0 +1,42 @@
+<?php
+require_once '../bot/api.php';
+class UserNotFoundException extends Exception {}
+class User {
+    private int $id;
+    private ?int $telegram_id;
+    private int $phone;
+
+    function __construct(int $telegram_id, bool $createuser = false, ?int $phone_number = NULL) {
+        global $sql;
+        $q = $sql->query("SELECT * FROM users WHERE telegram_id = '$telegram_id'") or die($sql->error);
+        if ($q->num_rows != 1) {
+            if ($createuser) {
+                if (is_null($phone_number)) throw new InvalidArgumentException("Phone number is NULL");
+                $q = $sql->query("SELECT * FROM users WHERE phone = '$phone_number'");
+                if ($q->num_rows != 1) throw new Exception("User has no ability to use bot!");
+                $sql->query("UPDATE users SET telegram_id = '$telegram_id' WHERE phone = '$phone_number'");
+            }
+            else throw new UserNotFoundException("User is not found!");
+        }
+        $d = $q->fetch_assoc();
+        $this->telegram_id = $telegram_id;
+        $this->id = $d['id'];
+        $this->phone = $d['phone'];
+    }
+
+    public function getID(): int {
+        return $this->id;
+    }
+
+    public function getTelegramID(): int {
+        return $this->telegram_id;
+    }
+
+    public function getPhone(): int {
+        return $this->phone;
+    }
+
+    public function sendNotification(string $text): void {
+        API::sendMessage($this->id, $text, 1);
+    }
+}
